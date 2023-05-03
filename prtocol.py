fbb6c012d11283912f5f85ed1716923c871f9ae7
AndreasBorum <andreas.bo.po@gmail.com>
2023-05-03 13:21:16 +0200

    protocol

---
+++ prtocol.py
@@ -0,0 +1,18 @@
+"""
+
+
+(start_data, instruktioner)
+
+start_data = ( 2, 5, 1, 3, 7, 8, 9, x, x)
+
+
+instruktioner ((type, i1, i2), (type, i1, i2))
+
+typer:
+1- sammenlign 
+2- byt 
+3- færdig
+
+
+
+"""
