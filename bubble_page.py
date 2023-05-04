1436688f92819618f0c0b298e15d662619fbe144
AndreasBorum <andreas.bo.po@gmail.com>
2023-05-04 12:03:12 +0200

    maximized

---
+++ bubble_page.py
@@ -0,0 +1,20 @@
+import customtkinter as ctk
+import  tkinter as tk
+import style
+import long_text
+
+class BubblesortPage(ctk.CTkFrame):
+    def __init__(self, app):
+        super().__init__(app.right_side_panel, **style.frame_style)
+
+        self.description_frame = tk.Frame(self)
+        self.description_frame.grid(row=0, column=0, pady=10, padx=10)
+
+        self.animation_frame = tk.Frame(self)
+        self.animation_frame.grid(row=0, column=1, pady=10, padx=10)
+
+        self.header_label = ctk.CTkLabel(self.description_frame, text="Bubble sort", text_color="black", font=ctk.CTkFont(size=20, weight="bold"))
+        self.header_label.grid(row=0, column=0, pady=10, padx=10)
+
+        self.description_label = ctk.CTkLabel(self.description_frame, text=long_text.home_page_text, text_color="black", font=ctk.CTkFont(size=14), wraplength=200, justify=tk.CENTER)
+        self.description_label.grid(row=1, column=0, pady=10, padx=10)
