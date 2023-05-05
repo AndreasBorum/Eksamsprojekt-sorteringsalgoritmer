9ed445a72ead561e2d1def69d8a7a50dbd811df2
AndreasBorum <andreas.bo.po@gmail.com>
2023-05-05 15:37:55 +0200

    update protocol

---
+++ animation/prtocol.py
@@ -0,0 +1,29 @@
+"""
+
+
+(start_data, instruktioner)
+
+start_data = ( 2, 5, 1, 3, 7, 8, 9, x, x)
+
+
+instruktioner ((type, i1, i2), (type, i1, i2))
+
+typer:
+0- sammenlign 
+1- byt 
+3- færdig
+
+
+
+CanvasLogic
+
+
+
+
+
+
+
+
+
+
+"""
